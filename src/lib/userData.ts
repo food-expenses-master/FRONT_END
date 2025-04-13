ff0e8a56bfa52db97e43e70fc8b94ab7eaea63cd
HWANBINYOO <dngh0825@naver.com>
2025-04-13 20:01:50 +0900

    init : firebase setting

---
+++ src/lib/userData.ts
@@ -0,0 +1,12 @@
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "./firebase";
+
+export const getUserData = async (uid: string) => {
+  const docRef = doc(db, "users", uid);
+  const docSnap = await getDoc(docRef);
+  if (docSnap.exists()) {
+    return docSnap.data();
+  } else {
+    return null;
+  }
+};
